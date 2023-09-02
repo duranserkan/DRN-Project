b9899f92ea00bcab5fb674c460404ce5a6a18c8d
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2023-09-02 18:36:20 +0300

    New DockerFile and Program.cs structure added

--- Sample.Hosted/Program.cs
+++ Sample.Hosted/Program.cs
@@ -1,26 +1,65 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿//https://learn.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host
+//https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/program-structure/top-level-statements
+//https://learn.microsoft.com/en-us/ef/core/cli/dbcontext-creation
+//https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60 new hosting model
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var app = CreateApp(args);
+    //log info
+    app.Run();
+}
+catch (Exception exception)
+{
+    //log error
+}
+finally
+{
+    //log shutdown
 }
 
-app.UseHttpsRedirection();
+static WebApplication CreateApp(string[] args)
+{
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
+    var serviceCollection = builder.Services;
+    AddServices(serviceCollection, configuration);
 
-app.UseAuthorization();
+    var app = builder.Build();
+    var serviceProvider = app.Services;
+    ValidateDependencies(serviceProvider, configuration);
 
-app.MapControllers();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
 
+    return app;
+}
+
+static void AddServices(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+static void ValidateDependencies(IServiceProvider services, IConfiguration configuration)
+{
+    //validate services, settings and dependencies
+}
+
+//Required by ef core migrations
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    var hostBuilder = WebApplication.CreateBuilder(args).Host;
+    hostBuilder.ConfigureServices((hostContext, services) => AddServices(services, hostContext.Configuration));
+
+    return hostBuilder;
+}
