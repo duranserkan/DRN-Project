f3ac9043834c44fdc7653ff14f5e4b07fe0398ed
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2023-12-21 00:48:14 +0300

    Ef command examples simplified

--- DRN.Framework.EntityFramework/Context/DesignTimeDbContextFactory.cs
+++ DRN.Framework.EntityFramework/Context/DesignTimeDbContextFactory.cs
@@ -7,15 +7,14 @@ namespace DRN.Framework.EntityFramework.Context;
 
 public class DesignTimeDbContextFactory<TContext> : IDesignTimeDbContextFactory<TContext>, IDesignTimeServices
     where TContext : DbContext
-
 {
     //for ef tools
     //dotnet tool install --global dotnet-ef
     //dotnet tool update
     //https://learn.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli
-    //from example project root
-    //dotnet ef migrations add --project Sample.Infra.csproj --context Sample.Infra.Repositories.QA.QAContext [MigrationName]
-    //dotnet ef database update --project Sample.Infra.csproj --context Sample.Infra.Repositories.QA.QAContext  -- "connectionString"
+    //from project root
+    //dotnet ef migrations add --context QAContext [MigrationName]
+    //dotnet ef database update --context QAContext  -- "connectionString"
     public TContext CreateDbContext(string[] args)
     {
         var contextName = typeof(TContext).Name;
