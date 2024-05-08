0d144377ec3af8e596b3950b490d511e8fa5bf66
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2024-05-08 22:42:40 +0300

    todo added

--- DRN.Framework.Hosting/DrnProgram/DrnProgramOptions.cs
+++ DRN.Framework.Hosting/DrnProgram/DrnProgramOptions.cs
@@ -2,6 +2,7 @@ namespace DRN.Framework.Hosting.DrnProgram;
 
 public class DrnProgramOptions
 {
+    //todo: improve usage 
     public bool UseHttpRequestLogger { get; set; }
     public DrnAppBuilderType AppBuilderType { get; set; } = DrnAppBuilderType.DrnDefaults;
 }
