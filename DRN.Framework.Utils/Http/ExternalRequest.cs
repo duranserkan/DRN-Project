4b13a544261e7c7236e0286a6b80ee030a4f7e5c
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2024-07-23 20:46:49 +0300

    External request added

---
+++ DRN.Framework.Utils/Http/ExternalRequest.cs
@@ -0,0 +1,32 @@
+using DRN.Framework.SharedKernel.Conventions;
+using DRN.Framework.Utils.DependencyInjection.Attributes;
+using Flurl;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+
+namespace DRN.Framework.Utils.Http;
+
+/// <summary>
+/// ExternalRequest request is a simple factory for your external http(s) request calls.
+/// <summary>
+public interface IExternalRequest
+{
+    IFlurlRequest For(Url endpoint, Version httpVersion);
+}
+
+[Singleton<IExternalRequest>]
+public class ExternalRequest : IExternalRequest
+{
+    private static readonly DefaultJsonSerializer JsonSerializer = new(JsonConventions.DefaultOptions);
+
+    public IFlurlRequest For(Url endpoint, Version httpVersion) => For(endpoint, httpVersion.ToString());
+
+    private static IFlurlRequest For(Url endpoint, string httpVersion)
+    {
+        return endpoint.WithSettings(x =>
+        {
+            x.HttpVersion = httpVersion;
+            x.JsonSerializer = JsonSerializer;
+        });
+    }
+}
