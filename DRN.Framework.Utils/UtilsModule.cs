0c3f9e89b6e0e20eace927af5e503e537a2d285d
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-03-15 12:00:14 +0300

    Nuget packages updated

--- DRN.Framework.Utils/UtilsModule.cs
+++ DRN.Framework.Utils/UtilsModule.cs
@@ -7,7 +7,7 @@ public static class UtilsModule
     public static IServiceCollection AddDrnUtils(this IServiceCollection collection)
     {
         collection.AddServicesWithAttributes();
-        collection.AddMemoryCache();
+        collection.AddHybridCache(); //todo: evaluate fusion cache
 
         return collection;
     }
