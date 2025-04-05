47dc602178f9d1f8f654b9bc93ca1d000153520f
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-04-05 19:59:30 +0300

    SourceKnownID bulk generation test added

--- DRN.Test.Unit/Tests/Framework/Utils/Common/SourceKnownIdTests.cs
+++ DRN.Test.Unit/Tests/Framework/Utils/Common/SourceKnownIdTests.cs
@@ -27,24 +27,92 @@ public class SourceKnownIdTests
 
         context.AddToConfiguration(customSettings);
         var generator = context.GetRequiredService<ISourceKnownIdGenerator>();
-        
+
         var epoch = IdGenerator.Epoch2025;
         var beforeIdGenerated = DateTimeOffset.UtcNow;
-        
+
         await Task.Delay(1000);
-        var id =  generator.NextId<SourceKnownIdTests>();
+        var id = generator.NextId<SourceKnownIdTests>();
         await Task.Delay(1000);
-        
+
         var afterIdGenerated = DateTimeOffset.UtcNow;
-        
+
         id.Should().BeNegative();
+        epoch.Should().BeBefore(beforeIdGenerated);
 
         var idInfo = generator.Parse(id);
         idInfo.AppId.Should().Be(nexusSettings.NexusAppId);
         idInfo.AppInstanceId.Should().Be(nexusSettings.NexusAppInstanceId);
-        
-        epoch.Should().BeBefore(beforeIdGenerated);
+
         idInfo.CreatedAt.Should().BeBefore(afterIdGenerated);
         idInfo.CreatedAt.Should().BeAfter(beforeIdGenerated);
+    }
+
+    [Theory]
+    [DataInlineUnit]
+    public async Task SourceKnownIDs_Should_Be_Generate_Ids_For_3_Seconds(UnitTestContext context)
+    {
+        var nexusSettings = new NexusAppSettings
+        {
+            NexusAppId = 7,
+            NexusAppInstanceId = 24
+        };
+
+        var customSettings = new
+        {
+            NexusAppSettings = nexusSettings
+        };
+
+        context.AddToConfiguration(customSettings);
+        var generator = context.GetRequiredService<ISourceKnownIdGenerator>();
+        var bucketCount = 3;
+        var idCount = ushort.MaxValue * bucketCount;
+        var idArray = new long[idCount];
+
+        var epoch = IdGenerator.Epoch2025;
+        var beforeIdGenerated = DateTimeOffset.UtcNow;
+
+        await Task.Delay(1000);
+        for (var i = 0; i < idCount; i++)
+            idArray[i] = generator.NextId<ISourceKnownIdGenerator>();
+        await Task.Delay(1000);
+
+        var afterIdGenerated = DateTimeOffset.UtcNow;
+
+        epoch.Should().BeBefore(beforeIdGenerated);
+
+        var idInfos = idArray.Select(id => generator.Parse(id)).ToArray();
+
+        idInfos.Length.Should().Be(idCount);
+        idInfos.Should().AllSatisfy(idInfo =>
+        {
+            idInfo.Id.Should().BeNegative();
+            idInfo.AppId.Should().Be(nexusSettings.NexusAppId);
+            idInfo.AppInstanceId.Should().Be(nexusSettings.NexusAppInstanceId);
+
+            idInfo.CreatedAt.Should().BeBefore(afterIdGenerated);
+            idInfo.CreatedAt.Should().BeAfter(beforeIdGenerated);
+        });
+
+        var idInfoGroups = idInfos.GroupBy(x => x.CreatedAt).ToArray();
+        var buckets = idInfoGroups.Select(x => x.Key).ToArray();
+
+        buckets.Length.Should().BeGreaterThanOrEqualTo(bucketCount);
+        buckets.Length.Should().BeLessThan(bucketCount + 1);
+
+        var duration = afterIdGenerated - beforeIdGenerated;
+        duration.TotalSeconds.Should().BeGreaterThanOrEqualTo(bucketCount);
+        duration.TotalSeconds.Should().BeLessThan(bucketCount + 1);
+
+        foreach (var group in idInfoGroups)
+        {
+            var groupCount = group.Count();
+            group.First().InstanceId.Should().Be(0);
+            if (groupCount > 1)
+                group.Skip(1).First().InstanceId.Should().Be(1);
+
+            group.Last().InstanceId.Should().Be((ushort)(groupCount - 1));
+        }
+        //todo measure performance, how fast they are generated?
     }
 }
