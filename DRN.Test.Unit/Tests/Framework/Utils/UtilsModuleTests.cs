11a059795480d4f287349cb2cbac2d33babb8752
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-04-24 21:59:06 +0300

    test fix

--- DRN.Test.Unit/Tests/Framework/Utils/UtilsModuleTests.cs
+++ DRN.Test.Unit/Tests/Framework/Utils/UtilsModuleTests.cs
@@ -1,6 +1,9 @@
 using DRN.Framework.Testing.Contexts;
 using DRN.Framework.Testing.DataAttributes;
 using DRN.Framework.Utils;
+using DRN.Framework.Utils.Settings;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace DRN.Test.Unit.Tests.Framework.Utils;
@@ -13,5 +16,9 @@ public class UtilsModuleTests
     {
         context.ServiceCollection.AddDrnUtils();
         context.ValidateServices();
+
+        var appSettings = context.GetRequiredService<IAppSettings>();
+        var key = appSettings.Nexus.GetDefaultMacKey();
+        key.Key.Should().Be("wN2dC5sO7vVkXpQnYqRtJbZaUxLmKoMhH8GfP4yEI0k=");
     }
 }
