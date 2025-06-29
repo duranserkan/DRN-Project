d2c142dcd5a96e0c8b0bf91e8e005198767d898e
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-06-29 12:05:10 +0300

    PaginationUtils refactored to reduce cognitive load

--- DRN.Framework.Utils/Entity/PaginationUtils.cs
+++ DRN.Framework.Utils/Entity/PaginationUtils.cs
@@ -19,14 +19,10 @@ public class PaginationUtils(ISourceKnownEntityIdUtils utils) : IPaginationUtils
         CancellationToken? cancellationToken = null) where TEntity : SourceKnownEntity
     {
         var ct = cancellationToken ?? CancellationToken.None;
-        var totalCount = request.Total.Count != -1 ? request.Total.Count : -1;
-
-        if (request.UpdateTotalCount)
-            totalCount = await query.LongCountAsync(ct);
-
         var sortDirection = request.PageCursor.SortDirection;
         var navigationDirection = request.NavigationDirection;
         var filteredQuery = query;
+        
         if (!request.PageCursor.IsFirstRequest)
         {
             //Valid SourceKnownIds carry created date information which is monotonic with drift protection
@@ -39,55 +35,84 @@ public class PaginationUtils(ISourceKnownEntityIdUtils utils) : IPaginationUtils
             if (!lastEntityId.Valid)
                 throw new ValidationException($"Invalid PaginationRequest.PageCursor.LastId: {request.PageCursor.LastId}");
 
-            if (navigationDirection == NavigationDirection.Refresh)
-            {
-                var firstId = firstEntityId.Source.Id;
-                var lastId = lastEntityId.Source.Id;
-                filteredQuery = sortDirection == PageSortDirection.AscendingByCreatedAt
-                    ? query.Where(entity => entity.Id >= firstId && entity.Id <= lastId)
-                    : query.Where(entity => entity.Id >= lastId && entity.Id <= firstId);
-            }
-            else if (navigationDirection == NavigationDirection.Next)
-            {
-                var cursorId = lastEntityId.Source.Id;
-                filteredQuery = sortDirection == PageSortDirection.AscendingByCreatedAt
-                    ? query.Where(entity => entity.Id > cursorId)
-                    : query.Where(entity => entity.Id < cursorId);
-            }
-            else //previous page
-            {
-                var cursorId = firstEntityId.Source.Id;
-                filteredQuery = sortDirection == PageSortDirection.AscendingByCreatedAt
-                    ? query.Where(entity => entity.Id < cursorId)
-                    : query.Where(entity => entity.Id > cursorId);
-            }
+            filteredQuery = FilterQueryByCursor(query, firstEntityId, lastEntityId, navigationDirection, sortDirection);
         }
 
-        IOrderedQueryable<TEntity> orderedQuery;
-        if (navigationDirection == NavigationDirection.Previous)
+        var orderedQuery = SortByEntityId(filteredQuery, navigationDirection, sortDirection);
+        var items = await GetEntitiesAsync(orderedQuery, request, navigationDirection, sortDirection, ct);
+
+        var totalCount = request.Total.Count != -1 ? request.Total.Count : -1;
+        if (request.UpdateTotalCount)
+            totalCount = await query.LongCountAsync(ct);
+
+        return new PaginationResult<TEntity>(items, request, totalCount);
+    }
+
+    private static IQueryable<TEntity> FilterQueryByCursor<TEntity>(IQueryable<TEntity> query, SourceKnownEntityId firstEntityId, SourceKnownEntityId lastEntityId,
+        NavigationDirection navigation, PageSortDirection sort) where TEntity : SourceKnownEntity
+    {
+        IQueryable<TEntity> filteredQuery;
+        if (navigation == NavigationDirection.Refresh)
         {
-            orderedQuery = sortDirection == PageSortDirection.DescendingByCreatedAt
+            var firstId = firstEntityId.Source.Id;
+            var lastId = lastEntityId.Source.Id;
+            filteredQuery = sort == PageSortDirection.AscendingByCreatedAt
+                ? query.Where(entity => entity.Id >= firstId && entity.Id <= lastId)
+                : query.Where(entity => entity.Id >= lastId && entity.Id <= firstId);
+        }
+        else if (navigation == NavigationDirection.Next)
+        {
+            var cursorId = lastEntityId.Source.Id;
+            filteredQuery = sort == PageSortDirection.AscendingByCreatedAt
+                ? query.Where(entity => entity.Id > cursorId)
+                : query.Where(entity => entity.Id < cursorId);
+        }
+        else //previous page
+        {
+            var cursorId = firstEntityId.Source.Id;
+            filteredQuery = sort == PageSortDirection.AscendingByCreatedAt
+                ? query.Where(entity => entity.Id < cursorId)
+                : query.Where(entity => entity.Id > cursorId);
+        }
+
+        return filteredQuery;
+    }
+
+    private static IOrderedQueryable<TEntity> SortByEntityId<TEntity>(IQueryable<TEntity> filteredQuery, NavigationDirection navigation, PageSortDirection sort)
+        where TEntity : SourceKnownEntity
+    {
+        IOrderedQueryable<TEntity> orderedQuery;
+        if (navigation == NavigationDirection.Previous)
+        {
+            orderedQuery = sort == PageSortDirection.DescendingByCreatedAt
                 ? filteredQuery.OrderBy(entity => entity.Id)
                 : filteredQuery.OrderByDescending(entity => entity.Id);
         }
         else
         {
-            orderedQuery = sortDirection == PageSortDirection.AscendingByCreatedAt
+            orderedQuery = sort == PageSortDirection.AscendingByCreatedAt
                 ? filteredQuery.OrderBy(entity => entity.Id)
                 : filteredQuery.OrderByDescending(entity => entity.Id);
         }
 
-        IReadOnlyList<TEntity> items = request.IsPageJump()
-            ? await orderedQuery.Skip(request.GetSkipSize()).Take(request.PageSize.Size + 1).ToListAsync(ct)
-            : await orderedQuery.Take(request.PageSize.Size + 1).ToListAsync(ct);
+        return orderedQuery;
+    }
 
-        if (navigationDirection == NavigationDirection.Previous)
+    private static async Task<IReadOnlyList<TEntity>> GetEntitiesAsync<TEntity>(IQueryable<TEntity> query, PaginationRequest request,
+        NavigationDirection navigation, PageSortDirection sort, CancellationToken cancellationToken)
+        where TEntity : SourceKnownEntity
+    {
+        IReadOnlyList<TEntity> items = request.IsPageJump()
+            ? await query.Skip(request.GetSkipSize()).Take(request.PageSize.Size + 1).ToListAsync(cancellationToken)
+            : await query.Take(request.PageSize.Size + 1).ToListAsync(cancellationToken);
+
+        if (navigation == NavigationDirection.Previous)
         {
-            items = sortDirection == PageSortDirection.AscendingByCreatedAt
+            items = sort == PageSortDirection.AscendingByCreatedAt
                 ? items.OrderBy(entity => entity.Id).ToArray()
                 : items.OrderByDescending(entity => entity.Id).ToArray();
         }
 
-        return new PaginationResult<TEntity>(items, request, totalCount);
+        return items;
     }
 }
