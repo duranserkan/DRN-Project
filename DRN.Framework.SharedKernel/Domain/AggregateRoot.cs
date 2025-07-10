8d8645617ac0f4dd2ac445042af16fd8e35a5f8c
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-07-10 22:22:57 +0300

    AggregateRoot model support added

--- DRN.Framework.SharedKernel/Domain/AggregateRoot.cs
+++ DRN.Framework.SharedKernel/Domain/AggregateRoot.cs
@@ -1,3 +1,5 @@
 namespace DRN.Framework.SharedKernel.Domain;
 
-public abstract class AggregateRoot : SourceKnownEntity;
+public abstract class AggregateRoot(long id = 0)  : SourceKnownEntity(id);
+
+public abstract class AggregateRoot<TModel>(long id = 0) : SourceKnownEntity<TModel>(id) where TModel : class;
