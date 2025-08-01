244ff5be31849a9413d1afa5daa3d0d891cf81eb
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-08-01 11:34:32 +0300

    TagController new usage cases added

--- Sample.Hosted/Controllers/QA/TagController.cs
+++ Sample.Hosted/Controllers/QA/TagController.cs
@@ -9,13 +9,45 @@ namespace Sample.Hosted.Controllers.QA;
 public class TagController(ITagRepository repository) : ControllerBase
 {
     [HttpPost("Pagination")]
-    public async Task<PaginationResultModel<Tag>> GetAsync([FromBody]PaginationRequest? request)
+    public async Task<PaginationResultModel<Tag>> GetAsync([FromBody] PaginationRequest? request)
     {
         var result = await repository.PaginateAsync(request ?? PaginationRequest.Default);
 
         return result;
     }
 
+    [HttpPost("Pagination/Next")]
+    public async Task<PaginationResultModel<Tag>> GetNextAsync([FromBody] PaginationResultInfo resultInfo)
+    {
+        var result = await repository.PaginateAsync(resultInfo.RequestNextPage());
+
+        return result;
+    }
+
+    [HttpPost("Pagination/Previous")]
+    public async Task<PaginationResultModel<Tag>> GetPreviousAsync([FromBody] PaginationResultInfo resultInfo)
+    {
+        var result = await repository.PaginateAsync(resultInfo.RequestPreviousPage());
+
+        return result;
+    }
+    
+    [HttpPost("Pagination/Refresh")]
+    public async Task<PaginationResultModel<Tag>> GetRefreshAsync([FromBody] PaginationResultInfo resultInfo)
+    {
+        var result = await repository.PaginateAsync(resultInfo.RequestRefresh());
+
+        return result;
+    }
+
+    [HttpPost("Pagination/Jump/{pageNumber:int}")]
+    public async Task<PaginationResultModel<Tag>> GetJumpAsync([FromBody] PaginationResultInfo resultInfo, [FromRoute] int pageNumber)
+    {
+        var result = await repository.PaginateAsync(resultInfo.RequestPage(pageNumber));
+
+        return result;
+    }
+    
     [HttpGet("{id:guid}")]
     public async Task<Tag> GetAsync([FromRoute] Guid id)
     {
@@ -35,5 +67,13 @@ public class TagController(ITagRepository repository) : ControllerBase
         await repository.CreateAsync(tag);
 
         return tag;
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<int> DeleteAsync([FromRoute] Guid id)
+    {
+        var count = await repository.DeleteAsync(id);
+
+        return count;
     }
 }
