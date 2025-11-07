4637e0a8fc6eeebcb72f6a31f12185cf4668decc
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-11-07 22:21:04 +0300

    Dto refactored

--- DRN.Framework.SharedKernel/Domain/Dto.cs
+++ DRN.Framework.SharedKernel/Domain/Dto.cs
@@ -1,16 +1,21 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace DRN.Framework.SharedKernel.Domain;
 
 public class Dto
 {
-    [JsonConstructor]
-    protected Dto()
-    {
-    }
+    [JsonIgnore] //JsonIgnore is added to prevent entity serialization
+    [JsonInclude] //JsonInclude is added to make constructor happy during deserialization
+#pragma warning disable CS0169
+    private SourceKnownEntity? _entity; // Field is never used, entity and _entity added to share same constructor with serializer and application code 
+#pragma warning restore CS0169
 
-    public Dto(SourceKnownEntity entity)
+    public Dto(SourceKnownEntity? entity = null)
     {
+        if (entity is null)
+            return;
+
         Id = entity.EntityId;
         CreatedAt = entity.CreatedAt;
         ModifiedAt = entity.ModifiedAt;
@@ -19,4 +24,7 @@ public class Dto
     public Guid Id { get; init; }
     public DateTimeOffset CreatedAt { get; init; }
     public DateTimeOffset ModifiedAt { get; init; }
+
+    [JsonExtensionData]
+    public Dictionary<string, JsonElement>? AdditionalData { get; init; }
 }
