a54db9677a4ecbfd60c02d71e0914b5fc9867b9c
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-11-23 20:42:08 +0300

    __entity_migrations renamed as entity_migrations

--- DRN.Framework.EntityFramework/Attributes/DrnContextDefaultsAttribute.cs
+++ DRN.Framework.EntityFramework/Attributes/DrnContextDefaultsAttribute.cs
@@ -19,7 +19,7 @@ public class DrnContextDefaultsAttribute : NpgsqlDbContextOptionsAttribute
     public override void ConfigureNpgsqlOptions<TContext>(NpgsqlDbContextOptionsBuilder builder, IServiceProvider? serviceProvider) => builder
         .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
         .MigrationsAssembly(typeof(TContext).Assembly.FullName)
-        .MigrationsHistoryTable($"{typeof(TContext).Name.ToSnakeCase()}_history", "__entity_migrations")
+        .MigrationsHistoryTable($"{typeof(TContext).Name.ToSnakeCase()}_history", "entity_migrations")
         .SetPostgresVersion(18,1);
 
     public override void ConfigureNpgsqlDataSource<TContext>(NpgsqlDataSourceBuilder builder, IServiceProvider serviceProvider)
