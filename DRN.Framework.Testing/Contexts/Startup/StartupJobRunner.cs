945c5a499547c6badd6fe5d5c28dd20bee821b8e
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2025-11-24 20:09:03 +0300

    Revert "//Ignored StartupContext - Npgsql.PostgresException : 42P01: relation "entity_migrations.{context}_history" does not exist"
    This reverts commit 3d1818a2e9f6588441d12ef2ff3b54e106d32d21.

--- DRN.Framework.Testing/Contexts/Startup/StartupJobRunner.cs
+++ DRN.Framework.Testing/Contexts/Startup/StartupJobRunner.cs
@@ -1,7 +1,6 @@
 using DRN.Framework.Utils.Configurations;
 using DRN.Framework.Utils.Extensions;
 using DRN.Framework.Utils.Settings;
-using Npgsql;
 
 namespace DRN.Framework.Testing.Contexts.Startup;
 
@@ -33,23 +32,15 @@ public static class StartupJobRunner
 
     private static void Trigger(MethodInfo testMethod)
     {
-        TestEnvironment.DrnTestContextEnabled = true;
         var startedAt = DateTimeOffset.Now;
-        var jobTypes = GetTestStartupJobTypes(testMethod);
+        TestEnvironment.DrnTestContextEnabled = true;
 
-        try
+        var jobTypes = GetTestStartupJobTypes(testMethod);
+        foreach (var startupJobType in jobTypes)
         {
-            foreach (var startupJobType in jobTypes)
-            {
-                var startupJob = (ITestStartupJob)Activator.CreateInstance(startupJobType)!;
-                using var startupContext = new StartupContext(startupJob);
-                startupJob.RunAsync(startupContext).GetAwaiter().GetResult();
-            }
-        }
-        catch (PostgresException ex)
-        {
-            //Ignored Npgsql.PostgresException : 42P01: relation "entity_migrations.{context}_history" does not exist
-            _ = ex;
+            var startupJob = (ITestStartupJob)Activator.CreateInstance(startupJobType)!;
+            using var startupContext = new StartupContext(startupJob);
+            startupJob.RunAsync(startupContext).GetAwaiter().GetResult();
         }
 
         Result = new TestStartupResult(startedAt, DateTimeOffset.Now, testMethod, jobTypes);
