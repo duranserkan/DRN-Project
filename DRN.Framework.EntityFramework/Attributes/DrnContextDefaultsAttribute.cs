252fd3dce44f27b5785764d82d86243f9c3aa2b7
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2026-02-13 20:30:02 +0300

    postgres and rabbitmq images updated

--- DRN.Framework.EntityFramework/Attributes/DrnContextDefaultsAttribute.cs
+++ DRN.Framework.EntityFramework/Attributes/DrnContextDefaultsAttribute.cs
@@ -20,7 +20,7 @@ public class DrnContextDefaultsAttribute : NpgsqlDbContextOptionsAttribute
         .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
         .MigrationsAssembly(typeof(TContext).Assembly.FullName)
         .MigrationsHistoryTable($"{typeof(TContext).Name.ToSnakeCase()}_history", "__entity_migrations")
-        .SetPostgresVersion(18,1);
+        .SetPostgresVersion(18,2);
 
     public override void ConfigureNpgsqlDataSource<TContext>(NpgsqlDataSourceBuilder builder, IServiceProvider serviceProvider)
     {
