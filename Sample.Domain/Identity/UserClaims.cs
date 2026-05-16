12ab071346c884d11e726cc0dd51d609457461b9
Duran Serkan KILIÇ <25037337+duranserkan@users.noreply.github.com>
2026-05-16 20:13:15 +0300

    feat(Sample): integrate rate limiting features

--- Sample.Domain/Identity/UserClaims.cs
+++ Sample.Domain/Identity/UserClaims.cs
@@ -2,6 +2,8 @@ namespace Sample.Domain.Identity;
 
 public abstract class UserClaims
 {
+    public const string AccountId = nameof(AccountId);
+    public const string TenantId = nameof(TenantId);
     public const string PPVersion = nameof(PPVersion);
     public const string SlimUI = nameof(SlimUI);
 }
